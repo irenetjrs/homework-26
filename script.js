738c01734a7d61aac44cdf6da20ccae9e0b7ed2f
irenetjrs <irkapidgorodetska@gmail.com>
2023-02-24 15:54:31 +0200

    task 2

--- script.js
+++ script.js
@@ -22,3 +22,23 @@ let toyCar = {
 }
 Object.setPrototypeOf(toyCar, sportCar);
 console.log(toyCar);
+
+// task 2
+
+let employees = {
+   wallet: {},
+   pay(month, sum){
+      this.wallet[month] = sum;
+   }
+}
+let frontendDeveloper = Object.create (employees, employees.pay);
+   frontendDeveloper.name = 'Mike';
+   frontendDeveloper.wallet = {};
+
+let backendDeveloper = Object.create (employees, employees.pay);
+   backendDeveloper.name = 'Bob';
+   backendDeveloper.wallet = {};
+
+   backendDeveloper.pay('june', 1500);
+console.log(backendDeveloper.wallet.june);
+console.log(frontendDeveloper.wallet.june);
